a16cc36fa092302401e458688a134aff62a33806
Ibrahim Abdulaziz <ibrahimabdullaziz55@gmail.com>
2026-04-30 02:28:00 +0300

    connect database and add views

---
+++ app.js
@@ -0,0 +1,38 @@
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+const express = require("express");
+const app = express();
+
+const { hostname } = require("os");
+const path = require("path");
+
+require("dotenv").config();
+const port = process.env.LISTENING_PORT;
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((result) => {
+    console.log("DATABASE CONNECTED SUCCESSFULLY");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  return res.render("index", {
+    pageTitle: "Hello!",
+    path: "/",
+  });
+});
+
+// console.log(process.env.LISTENING_PORT);
+app.listen(port, hostname, () => {
+  console.log(`LISTENING ON ${hostname} ${port}`);
+});
